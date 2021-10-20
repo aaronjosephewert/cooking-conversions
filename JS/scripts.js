b960a971a00b947c6cd5d08a87bd812428f5fbe4
Aaron Ewert <aaronjewert@gmail.com>
2021-10-20 16:32:30 -0700

    add rest of js
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,46 @@
 function flTbsp(tbsp){
-  return tbsp * 2;
+  return tbsp / 2;
 }
 
 function flozCup (floz){
-return floz * 8;
+return floz / 8;
 }
 
 function cupPint (cup){
-  return cup * 2;
+  return cup / 2;
 }
 
 function pintQuart (pint){
   return pint * 2;
 }
+
+$(document).ready(function() {
+$("form#tablespoon").submit(function(event){
+event.preventDefault();
+const tbsp = parseInt($("#tbsp").val());
+const result = flTbsp(tbsp);
+$("#output").text(result);
+})
+
+$("form#fluid-ounce").submit(function(event){
+  event.preventDefault();
+  const floz = parseInt($("#fl-oz").val());
+  const result = flozCup(floz);
+  $("#output").text(result);
+  })
+
+  $("form#cups").submit(function(event){
+    event.preventDefault();
+    const george = parseInt($("#cups").val());
+    const result = cupPint(george);
+    $("#output").text(result);
+    })
+
+    $("form#pints").submit(function(event){
+      event.preventDefault();
+      const george = parseInt($("#pints").val());
+      const result = pintQuart(george);
+      $("#output").text(result);
+      })
+})
+
