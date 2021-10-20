3b8ec5f6af94449da6161d0b47d1863bf15057a5
Aaron Ewert <aaronjewert@gmail.com>
2021-10-20 16:12:01 -0700

    add css and some js
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

---
+++ JS/scripts.js
@@ -0,0 +1,15 @@
+function flTbsp(tbsp){
+  return tbsp * 2;
+}
+
+function flozCup (floz){
+return floz * 8;
+}
+
+function cupPint (cup){
+  return cup * 2;
+}
+
+function pintQuart (pint){
+  return pint * 2;
+}
